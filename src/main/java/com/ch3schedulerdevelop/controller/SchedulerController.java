5befd3a3e96f1188a5c5847936b25ef20dafa04b
Jaegu-Kang <rkdworn8@gmail.com>
2026-04-16 23:27:12 +0900

    일정 생성 POST 작성

--- src/main/java/com/ch3schedulerdevelop/controller/SchedulerController.java
+++ src/main/java/com/ch3schedulerdevelop/controller/SchedulerController.java
@@ -1,12 +1,26 @@
 package com.ch3schedulerdevelop.controller;
 
+import com.ch3schedulerdevelop.dto.CreateSchedulerRequest;
+import com.ch3schedulerdevelop.dto.CreateSchedulerResponse;
 import com.ch3schedulerdevelop.service.SchedulerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/schedulers")
 public class SchedulerController {
 
     private final SchedulerService schedulerService;
+
+    @PostMapping
+    public ResponseEntity<CreateSchedulerResponse> saveScheduler(@RequestBody CreateSchedulerRequest request){
+        CreateSchedulerResponse result = schedulerService.saveScheduler(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
 }
--- src/main/java/com/ch3schedulerdevelop/service/SchedulerService.java
+++ src/main/java/com/ch3schedulerdevelop/service/SchedulerService.java
@@ -1,12 +1,35 @@
 package com.ch3schedulerdevelop.service;
 
+import com.ch3schedulerdevelop.dto.CreateSchedulerRequest;
+import com.ch3schedulerdevelop.dto.CreateSchedulerResponse;
+import com.ch3schedulerdevelop.entity.Scheduler;
 import com.ch3schedulerdevelop.repository.SchedulerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class SchedulerService {
 
     private final SchedulerRepository schedulerRepository;
+
+    @Transactional
+    public CreateSchedulerResponse saveScheduler(CreateSchedulerRequest request){
+        Scheduler scheduler = new Scheduler(
+                request.getName(),
+                request.getTitle(),
+                request.getContent(),
+                request.getPassword()
+        );
+        Scheduler savedScheduler = schedulerRepository.save(scheduler);
+        return new CreateSchedulerResponse(
+                savedScheduler.getId(),
+                savedScheduler.getName(),
+                savedScheduler.getTitle(),
+                savedScheduler.getContent(),
+                savedScheduler.getCreatedAt(),
+                savedScheduler.getModifiedAt()
+        );
+    }
 }
