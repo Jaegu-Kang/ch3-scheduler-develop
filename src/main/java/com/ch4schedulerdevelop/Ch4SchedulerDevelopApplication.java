46e11da421fcc80ba0f9148a9002375f5029f194
Jaegu-Kang <rkdworn8@gmail.com>
2026-04-16 17:17:43 +0900

    Ch4 과제 준비

---
+++ src/main/java/com/ch4schedulerdevelop/Ch4SchedulerDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.ch4schedulerdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4SchedulerDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch4SchedulerDevelopApplication.class, args);
+    }
+
+}
