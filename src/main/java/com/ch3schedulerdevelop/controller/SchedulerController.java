221719f0d6930efc04efed8e7fdf1bc1747ecda8
Jaegu-Kang <rkdworn8@gmail.com>
2026-04-16 23:39:02 +0900

    전체일정 조회 GET 작성

--- src/main/java/com/ch3schedulerdevelop/controller/SchedulerController.java
+++ src/main/java/com/ch3schedulerdevelop/controller/SchedulerController.java
@@ -2,14 +2,14 @@ package com.ch3schedulerdevelop.controller;
 
 import com.ch3schedulerdevelop.dto.CreateSchedulerRequest;
 import com.ch3schedulerdevelop.dto.CreateSchedulerResponse;
+import com.ch3schedulerdevelop.dto.GetAllSchedulerResponse;
 import com.ch3schedulerdevelop.service.SchedulerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +22,11 @@ public class SchedulerController {
     public ResponseEntity<CreateSchedulerResponse> saveScheduler(@RequestBody CreateSchedulerRequest request){
         CreateSchedulerResponse result = schedulerService.saveScheduler(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetAllSchedulerResponse>> getAllScheduler(){
+        List<GetAllSchedulerResponse> result = schedulerService.getAllSchedulerResponse();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
--- src/main/java/com/ch3schedulerdevelop/service/SchedulerService.java
+++ src/main/java/com/ch3schedulerdevelop/service/SchedulerService.java
@@ -2,11 +2,15 @@ package com.ch3schedulerdevelop.service;
 
 import com.ch3schedulerdevelop.dto.CreateSchedulerRequest;
 import com.ch3schedulerdevelop.dto.CreateSchedulerResponse;
+import com.ch3schedulerdevelop.dto.GetAllSchedulerResponse;
 import com.ch3schedulerdevelop.entity.Scheduler;
 import com.ch3schedulerdevelop.repository.SchedulerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,5 +35,24 @@ public class SchedulerService {
                 savedScheduler.getCreatedAt(),
                 savedScheduler.getModifiedAt()
         );
+    }
+
+    @Transactional(readOnly = true)
+    public List<GetAllSchedulerResponse> getAllSchedulerResponse(){
+        List<Scheduler> schedulers = schedulerRepository.findAll();
+        List<GetAllSchedulerResponse> dtos = new ArrayList<>();
+
+        for (Scheduler scheduler : schedulers){
+            GetAllSchedulerResponse dto = new GetAllSchedulerResponse(
+                    scheduler.getId(),
+                    scheduler.getName(),
+                    scheduler.getTitle(),
+                    scheduler.getContent(),
+                    scheduler.getCreatedAt(),
+                    scheduler.getModifiedAt()
+            );
+            dtos.add(dto);
+        }
+        return dtos;
     }
 }
