f51728b986fc2ccbafb4118351b1d604715fa328
Jaegu-Kang <rkdworn8@gmail.com>
2026-04-20 17:25:30 +0900

    🎉 add 유저 전체조회 GET 작성
    LV2 유저 CRUD 구현중
    유저 전체조회 기능 작성

--- src/main/java/com/ch3schedulerdevelop/user/controller/UserController.java
+++ src/main/java/com/ch3schedulerdevelop/user/controller/UserController.java
@@ -2,14 +2,14 @@ package com.ch3schedulerdevelop.user.controller;
 
 import com.ch3schedulerdevelop.user.dto.CreateUserRequest;
 import com.ch3schedulerdevelop.user.dto.CreateUserResponse;
+import com.ch3schedulerdevelop.user.dto.GetAllUserResponse;
 import com.ch3schedulerdevelop.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +22,11 @@ public class UserController {
     public ResponseEntity<CreateUserResponse> saveUser(@RequestBody CreateUserRequest request){
         CreateUserResponse result = userService.saveUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetAllUserResponse>> getAllUser(){
+        List<GetAllUserResponse> result = userService.getAllUserResponse();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
--- src/main/java/com/ch3schedulerdevelop/user/service/UserService.java
+++ src/main/java/com/ch3schedulerdevelop/user/service/UserService.java
@@ -2,11 +2,14 @@ package com.ch3schedulerdevelop.user.service;
 
 import com.ch3schedulerdevelop.user.dto.CreateUserRequest;
 import com.ch3schedulerdevelop.user.dto.CreateUserResponse;
+import com.ch3schedulerdevelop.user.dto.GetAllUserResponse;
 import com.ch3schedulerdevelop.user.entity.User;
 import com.ch3schedulerdevelop.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +25,14 @@ public class UserService {
         );
         User savedUser = userRepository.save(user);
         return CreateUserResponse.from(savedUser);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GetAllUserResponse> getAllUserResponse(){
+        List<User> users = userRepository.findAll();
+
+        return users.stream()
+                .map(GetAllUserResponse::from)
+                .toList();
     }
 }
