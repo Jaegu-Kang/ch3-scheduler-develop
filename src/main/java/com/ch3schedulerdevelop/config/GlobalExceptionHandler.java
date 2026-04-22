0b6f55777945cc54be39faa8f0e860fc72b3dcf7
Jaegu-Kang <rkdworn8@gmail.com>
2026-04-22 15:34:45 +0900

    ✨ update GlobalExceptionHandler에 유효성 검사 예외 처리 추가
    클라이언트에게 유효성 검사 실패 상황을 전달하기 위해 대응 로직 구현

--- src/main/java/com/ch3schedulerdevelop/config/GlobalExceptionHandler.java
+++ src/main/java/com/ch3schedulerdevelop/config/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.ch3schedulerdevelop.config;
 
 import com.ch3schedulerdevelop.exception.ServerException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,5 +18,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(ex.getStatus())
                 .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(
+                error -> errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
