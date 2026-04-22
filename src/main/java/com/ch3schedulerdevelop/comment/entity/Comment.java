b65fad632b556ebb90ec839ba0599b6e163ebd4d
Jaegu-Kang <rkdworn8@gmail.com>
2026-04-22 17:29:09 +0900

    🎉 add 댓글 CRUD
    Lv 7 기능 구현을 위한
    Comment,CommentController
    CommentRepository,CommentService
    생성

---
+++ src/main/java/com/ch3schedulerdevelop/comment/entity/Comment.java
@@ -0,0 +1,30 @@
+package com.ch3schedulerdevelop.comment.entity;
+
+import com.ch3schedulerdevelop.scheduler.entity.BaseEntity;
+import com.ch3schedulerdevelop.scheduler.entity.Scheduler;
+import com.ch3schedulerdevelop.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String content;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "scheduler_id", nullable = false)
+    private Scheduler scheduler;
+
+}
---
+++ src/main/java/com/ch3schedulerdevelop/comment/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.ch3schedulerdevelop.comment.service;
+
+import com.ch3schedulerdevelop.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+}
